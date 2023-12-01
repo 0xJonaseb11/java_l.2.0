54eeb4839fe8b34ab7c5750829fa7347ff47e602
jonas-sebera <sebejaz99@gmail.com>
2023-12-01 15:50:15 +0200

    update list

--- Collections/ListManip.java
+++ Collections/ListManip.java
@@ -21,3 +21,26 @@ public class ListManipulation {
 
         // Update Faith to "Faith Mbabazi"
         
+        int faithIndex = list2.indexOf("Faith");
+        if (faithIndex != -1) {
+            list2.set(faithIndex, "Faith Mbabazi");
+        }
+
+        // Remove Mustapha
+        
+        list2.remove("Mustapha");
+
+        // Check if we have Alice
+        
+        boolean hasAlice = list1.contains("Alice");
+        System.out.println("Does list1 contain Alice? " + hasAlice);
+
+        // Add the second list to list1
+        
+        list1.addAll(list2);
+
+        // Print the modified lists
+        System.out.println("Modified list1: " + list1);
+        System.out.println("Modified list2: " + list2);
+    }
+}
