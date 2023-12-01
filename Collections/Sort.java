fc40c365c4cc4de71e676da78e4bdd74f2c8ddd8
jonas-sebera <sebejaz99@gmail.com>
2023-12-01 15:59:48 +0200

    update sort

--- Collections/Sort.java
+++ Collections/Sort.java
@@ -1,3 +1,12 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+public class SortNumbers {
+    public static void main(String[] args) {
+
+        //list of numbers
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1)
+    }
+}
