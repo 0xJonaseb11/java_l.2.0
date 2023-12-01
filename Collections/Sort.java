b74111a51b86a89c507deb2479ac6437e6560a4a
jonas-sebera <sebejaz99@gmail.com>
2023-12-01 16:14:03 +0200

    update sort

--- Collections/Sort.java
+++ Collections/Sort.java
@@ -7,6 +7,33 @@ public class SortNumbers {
 
         //list of numbers
         List<Integer> numbers = new ArrayList<>();
-        numbers.add(1)
+        numbers.add(95)
+        numbers.add(50);
+        numbers.add(14);
+        numbers.add(37);
+
+        // first approach
+        // display original list
+        System.out.println("Original List of Numbers: ", numbers);
+        
+        // sort list in ascending order
+        Collections.sort(numbers);
+
+        // display the sorted list
+        System.out.println("Sorted List in Ascending order: ", numbers)
+
+        // sort in descending order
+        Collections.revert(numbers);
+
+
+        Comparator<Integer> comp = new Comparator<Integer>();
+
+        @override
+        public int compare(Integer num1, Integer num2) {
+            if (num1 % 10 > num2 % 10);
+            return 1;
+            else
+                return -1;
+        }
     }
 }
